221767c6659d64bf6bb9baac99f8e2feace13023
Yomy Rodríguez <yomyrg.91@gmail.com>
2024-07-31 12:27:07 +0700

    mudanças 31-7-2024 Yomy

---
+++ HotelBooking.Model/Rooms.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Model
+{
+    public class Rooms
+    {
+        public int Id { get; set; }
+        public string Clasic { get; set; }
+        public string Superior { get; set; }
+       
+        public string Premium { get; set; }
+        public string SuperiorDeluxe {  get; set; }
+        public string SuperiorFamily {  get; set; }
+
+
+
+
+
+    }
+}
---
+++ HotelBooking.Model/Suite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Model
+{
+    public class Suite
+    {
+        public int Id { get; set; } 
+        public string Junior {  get; set; } 
+        public string Prestige { get; set; }
+        public string FamilyJunior { get; set; }    
+
+    }
+}
